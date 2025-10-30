a69be84748c06dae28e66f98b4d6a855ddbf9cb4
eeshaanbharadwaj <1by22is053@bmsit.in>
2025-10-30 08:39:09 +0530

    deployment done

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 // Configure CORS for the frontend running on port 5173
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: "https://devecho-frontend.vercel.app/"
 }));
 
 //updated for clerk
